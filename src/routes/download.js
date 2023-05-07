d2606b64b7673b5dd0ab9a4365eee9807cdeaab9
Tim Gabrikowski <timgabrikowski@outlook.de>
2023-05-07 17:52:40 +0200

    download finnish

--- src/routes/download.js
+++ src/routes/download.js
@@ -1,16 +1,15 @@
 import { Router } from "express";
 import { File, Tag, MetaData } from "../db.js";
-import * as logger from "../logger.js";
-import { Op, where } from "sequelize";
 import * as _ from "underscore";
-import * as mime from "mime-types";
 
 const router = Router();
 
 router.get("/:id", async (req, res) => {
-	const id = req.query.id;
-
-	let file = File.findByPk(id);
+	const id = req.params.id;
+	let file = await File.findByPk(id);
+	if (!file) return res.sendStatus(404);
+	const filepath = file.dataValues.path;
+	res.download(filepath, file.name);
 });
 
 export const downloadRouter = router;
